b52873751690d5b38ed4b3f168626d08e7853688
Afsal.MS <afsal.ms@iwaresolutions.net>
2024-09-07 21:34:27 +0530

    Add project files.

---
+++ Member.Api/Controllers/MemberController.cs
@@ -0,0 +1,21 @@
+﻿using Member.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Member.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly IMemberServices _memberServices;
+        public MemberController(IMemberServices memberServices) { 
+            _memberServices = memberServices;
+        }
+        [HttpGet]
+        public ActionResult<IList<Domain.Member>> Get()
+        {
+            return Ok(_memberServices.GetAllMembers());
+        }
+    }
+}
---
+++ Member.Application/MemberServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Member.Application
+{
+    public class MemberServices:IMemberServices
+    {
+        private readonly IMemberRepository _memberRepository;
+        public MemberServices(IMemberRepository memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+
+        public List<Domain.Member> GetAllMembers()
+        {
+            return _memberRepository.GetAllMembers();
+        }
+    }
+}
